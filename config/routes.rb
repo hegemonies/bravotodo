9478ff5ada92051c9b8f4e8d0b734be3c2c5ffc0
AlexanderSenchenko <zayzsacha1998@gmail.com>
2019-12-01 17:15:34 +0700

    [ror] #4-ror: Fixed data model of todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :todos
   get 'welcome/index'
 
   root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_30_160156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.boolean "done"
+    t.string "description"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
