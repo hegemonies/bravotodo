efe6282ea336647a184364c2d9096b83760486a0
hegemonies <hegemonies@yandex.ru>
2019-12-15 20:37:49 +0700

    Revert "fixes"
    This reverts commit 1f3fb2a0a40e7532c387e670e52d9dcb6759f2e5.

--- docker/resources/entrypoint.sh
+++ docker/resources/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sleep 3
+sleep 5
 rm -rf /mnt/vendor
 rm /mnt/Gemfile.lock
 cp -r vendor /mnt/
 cp Gemfile.lock /mnt/
-
 bin/rails db:migrate
-bin/rails db:seed
 bin/rails server -b 0.0.0.0
