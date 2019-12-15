e629579b0893062f6f4c3d7a5e5694d6947c528a
hegemonies <hegemonies@yandex.ru>
2019-12-15 22:05:08 +0700

    New fixes

--- app/admin/todos.rb
+++ app/admin/todos.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Todo do
   permit_params :done, :description, :date, :user_id
+
+  form title: 'Add new todo' do |f|
+    f.semantic_errors
+    inputs 'Info' do
+      f.input :user
+
+      f.input :description
+    end
+    f.actions
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register User do
   permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
