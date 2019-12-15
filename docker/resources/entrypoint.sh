1f3fb2a0a40e7532c387e670e52d9dcb6759f2e5
hegemonies <hegemonies@yandex.ru>
2019-12-15 20:37:49 +0700

    fixes

--- docker/resources/entrypoint.sh
+++ docker/resources/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sleep 5
+sleep 3
 rm -rf /mnt/vendor
 rm /mnt/Gemfile.lock
 cp -r vendor /mnt/
 cp Gemfile.lock /mnt/
+
 bin/rails db:migrate
+bin/rails db:seed
 bin/rails server -b 0.0.0.0
